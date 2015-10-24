fee0b62cb9ad81d3bf8e1466ac0b80e3615ecbf8
Laurent <laurentt96@outlook.fr>
2015-10-24 14:25:26 +0200

    Connexion avec la bdd

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 	session_start(); //On initialise la session
-	
+
 	include('controllers/MainController.class.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-	<head> 
+	<head>
 		<meta charset="utf-8">
 		<title>TP3 - Connexion - TOSON/GERMAN</title>
 		<link rel="shortcut icon" href="img/favicon.ico" />
 		<link href="css/bootstrapM.css" type="text/css" rel="stylesheet" media="all"  />
 		<script src="js/jquery-1.7.2.min.js"></script>
 		<script src="js/bootstrap.js"></script>
-	</head> 
+	</head>
 	<body>
 		<?php
 
 			$controller = new MainControleur();
-			
-		?>		
+
+		?>
 	</body>
 </html>
 
--- models/Utilisateur.class.php
+++ models/Utilisateur.class.php
@@ -1,42 +1,80 @@
-<?php 
+<?php
 class Utilisateur {
 	private $_login;
 	private $_password;
 
 
 	function __construct($login, $password) {
-		$usernames = array('admin' => 'toto', 'Laurent' => 'tata', 'Arnaud' => 'titi');
+		//$usernames = array('admin' => 'toto', 'Laurent' => 'tata', 'Arnaud' => 'titi');
+		if($bdd = mysqli_connect('localhost', 'root', '', 'german_toson_webserv'))
+		{
+		}
+		else
+		{
+			echo 'Erreur';
+		}
 
 		if(!empty($login) AND !empty($password)) // On teste si les champs sont vides
 		{
-			if(array_key_exists($login, $usernames)) // On teste si le nom d'utilisateur entré par le visiteur existe dans le tableau
-			{
-				if ($usernames[$login] == $password) // On compare ensuite si le mot de passe entré correspond au nom d'utilisateur entré par le visiteur
-				{					
+			try{
+				$resultat = mysqli_query($bdd, 'SELECT * FROM users WHERE login=\'' .$login. '\'');
+				$log = mysqli_fetch_assoc($resultat);}
+			catch(Exception $e){
+				$_SESSION['error'] = 'Erreur requete BDD';
+				$_SESSION['display_msg_error'] = true;
+			}
+
+			if( mysqli_num_rows($resultat)=='0'){
+				$_SESSION['error'] = 'Login inconnu';
+				$_SESSION['display_msg_error'] = true;
+			}
+			else{
+				if($password==$log['password']){
+					$_SESSION['login'] = $log['id'];
 					$this->_login = $login;
 					$_SESSION['username'] = $login;
-					
 					$this->_password = $password;
-					
-					$_SESSION['isLogged'] = true;					
-					
+					$_SESSION['isLogged'] = true;
 					$cookie_value = $login.' '.$password;
 					setcookie("moncookie", $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 jour
 				}
-				else
-				{
-					$_SESSION['error'] = 'Mot de passe incorrect';
+				else{
+					$_SESSION['error'] = 'Mauvais mot de passe';
 					$_SESSION['display_msg_error'] = true;
 				}
 			}
-			else
-			{
-				$_SESSION['error'] = 'Utilisateur incorrect';
-				$_SESSION['display_msg_error'] = true;
-			}
+			// if(array_key_exists($login, $usernames)) // On teste si le nom d'utilisateur entré par le visiteur existe dans le tableau
+			// {
+			// 	if ($usernames[$login] == $password) // On compare ensuite si le mot de passe entré correspond au nom d'utilisateur entré par le visiteur
+			// 	{
+			// 		$this->_login = $login;
+			// 		$_SESSION['username'] = $login;
+			//
+			// 		$this->_password = $password;
+			//
+			// 		$_SESSION['isLogged'] = true;
+			//
+			// 		$cookie_value = $login.' '.$password;
+			// 		setcookie("moncookie", $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 jour
+			// 	}
+			// 	else
+			// 	{
+			// 		$_SESSION['error'] = 'Mot de passe incorrect';
+			// 		$_SESSION['display_msg_error'] = true;
+			// 	}
+			// }
+			// else
+			// {
+			// 	$_SESSION['error'] = 'Utilisateur incorrect';
+			// 	$_SESSION['display_msg_error'] = true;
+			// }
+		}
+		else {
+			$_SESSION['error'] = 'Formulaire vide';
+			$_SESSION['display_msg_error'] = true;
 		}
 	}
-	
+
 	// public function getLogin()
 	// {
 		// return $this->_login;
@@ -59,7 +97,7 @@ class Utilisateur {
 	// {
 		// $this->_password = $password;
 	// }
-	
+
 }
 
 ?>
