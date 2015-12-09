4a160cd646a133059cbd46d5a4e505be62b65612
Laurent <laurentt96@outlook.fr>
2015-12-09 08:30:03 +0100

    Update BDD

--- german_toson_webserv.sql
+++ german_toson_webserv.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 08 Décembre 2015 à 18:48
+-- Généré le :  Mer 09 Décembre 2015 à 08:29
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -31,8 +31,42 @@ CREATE TABLE IF NOT EXISTS `practices` (
   `name` varchar(255) COLLATE utf8_bin NOT NULL,
   `path` varchar(255) COLLATE utf8_bin NOT NULL,
   `user` varchar(30) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
+
+--
+-- Contenu de la table `practices`
+--
+
+INSERT INTO `practices` (`id`, `name`, `path`, `user`, `description`) VALUES
+(12, 'lol', '/webserv/practices/j2ee.pdf', 'admin', 'Cours sur le J2EE');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(10) unsigned NOT NULL,
+  `login` varchar(20) COLLATE utf8_bin NOT NULL,
+  `password` varchar(60) COLLATE utf8_bin NOT NULL,
+  `email` varchar(60) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `type`, `login`, `password`, `email`) VALUES
+(1, 1, 'admin', 'toto', 'admin@gmail.com'),
+(2, 2, 'Laurent', 'tata', 'laurentt96@outlook.fr'),
+(3, 2, 'Arnaud', 'titi', 'arnaud.german@gmail.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
